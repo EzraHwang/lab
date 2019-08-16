276fac926f5fc8f9077c715e73e1b4d5b1b80e52
Ezra <e1z2r3a4@gmail.com>
2019-08-16 14:31:50 +0800

    Change list to IEnumerable

--- CSharpAdvanceDesignTests/JoeySelectTests.cs
+++ CSharpAdvanceDesignTests/JoeySelectTests.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections;
-using ExpectedObjects;
+﻿using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using Lab;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -17,7 +17,7 @@ namespace CSharpAdvanceDesignTests
         {
             var urls = GetUrls();
 
-            var actual = JoeySelect(urls, url => url.Replace("http://", "https://"));
+            var actual = urls.JoeySelect(url => url.Replace("http://", "https://"));
             var expected = new List<string>
             {
                 "https://tw.yahoo.com",
@@ -29,13 +29,12 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
-
         [Test]
         public void append_port_9191_to_urls()
         {
             var urls = GetUrls();
 
-            var actual = JoeySelect(urls, url => $"{url}:9191");
+            var actual = urls.JoeySelect(url => $"{url}:9191");
             var expected = new List<string>
             {
                 "http://tw.yahoo.com:9191",
@@ -47,15 +46,81 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
-        private IEnumerable<string> JoeySelect(IEnumerable<string> urls, Func<string, string> seletor)
+        [Test]
+        public void select_with_seq_no()
         {
-            var result = new List<string>();
-            foreach (var url in urls)
-            {
-                result.Add(seletor(url));
-            }
+            var urls = GetUrls();
 
-            return result;
+            var actual = urls.JoeySelect((url, index) => Map(url, index));
+            var expected = new List<string>
+            {
+                "1. http://tw.yahoo.com",
+                "2. https://facebook.com",
+                "3. https://twitter.com",
+                "4. http://github.com",
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+
+        [Test]
+        public void get_full_name_of_employees()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "David", LastName = "Chen"}
+            };
+
+            var actual = employees.JoeySelect(e => $"{e.FirstName} {e.LastName}");
+            var expected = new[]
+            {
+                "Joey Chen",
+                "Tom Li",
+                "David Chen",
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+
+        [Test]
+        public void find_price_more_than_700_and_select_id_and_price()
+        {
+            var products = new List<Product>
+            {
+                new Product {Id = 1, Cost = 11, Price = 110, Supplier = "Odd-e"},
+                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"},
+                new Product {Id = 5, Cost = 51, Price = 510, Supplier = "Momo"},
+                new Product {Id = 6, Cost = 61, Price = 610, Supplier = "Momo"},
+                new Product {Id = 7, Cost = 71, Price = 710, Supplier = "Yahoo"},
+                new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
+            };
+
+            var actual = products
+                .JoeyWhere(product => product.Price > 700)
+                .JoeySelect(p => $"{p.Id}-{p.Price}");
+
+            foreach (var item in actual)
+            {
+                Console.WriteLine(item);
+            }
+            var expected = new[]
+            {
+                "7-710",
+                "8-780",
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private static string Map(string url, int index)
+        {
+            return $"{index}. {url}";
         }
 
         private static IEnumerable<string> GetUrls()
@@ -66,14 +131,5 @@ namespace CSharpAdvanceDesignTests
             yield return "http://github.com";
         }
 
-        private static List<Employee> GetEmployees()
-        {
-            return new List<Employee>
-            {
-                new Employee {FirstName = "Joey", LastName = "Chen"},
-                new Employee {FirstName = "Tom", LastName = "Li"},
-                new Employee {FirstName = "David", LastName = "Chen"}
-            };
-        }
     }
 }
--- Lab/LinqExtention.cs
+++ Lab/LinqExtention.cs
@@ -1,25 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using Lab.Entities;
 
 namespace Lab
 {
     public static class LinqExtention
     {
-        public static List<TResource> JoeyWhere<TResource>(this List<TResource> resources, Func<TResource, bool> predicate)
+        public static IEnumerable<TResource> JoeyWhere<TResource>(this IEnumerable<TResource> resources, Func<TResource, bool> predicate)
         {
-            var output = new List<TResource>();
-            foreach (var item in resources)
+            var enumerator = resources.GetEnumerator();
+            while (enumerator.MoveNext())
             {
+                var item = enumerator.Current;
                 if (predicate(item))
                 {
-                    output.Add(item);
+                    yield return item;
                 }
             }
+            //var output = new List<TResource>();
+            //foreach (var item in resources)
+            //{
+            //    if (predicate(item))
+            //    {
+            //        output.Add(item);
+            //    }
+            //}
 
-            return output;
+            //return output;
         }
 
-        public static List<TSource> JoeyWhereWithIndex<TSource>(this List<TSource> numbers, Func<TSource, int, bool> predicate)
+        public static IEnumerable<TSource> JoeyWhereWithIndex<TSource>(this IEnumerable<TSource> numbers, Func<TSource, int, bool> predicate)
         {
             var result = new List<TSource>();
             var index = 0;
@@ -30,6 +41,41 @@ namespace Lab
                     result.Add(number);
                 }
                 index++;
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<string> JoeySelect(this IEnumerable<string> urls, Func<string, string> selector)
+        {
+            return JoeySelect(urls, selector);
+            //var result = new List<string>();
+            //foreach (var url in urls)
+            //{
+            //    result.Add(selector(url));
+            //}
+
+            //return result;
+        }
+
+        public static IEnumerable<TResult> JoeySelect<TResource, TResult>(this IEnumerable<TResource> sources, Func<TResource, int, TResult> predicate)
+        {
+            var result = new List<TResult>();
+            var index = 1;
+            foreach (var resource in sources)
+            {
+                result.Add(predicate(resource, index));
+                index++;
+            }
+            return result;
+        }
+
+        public static IEnumerable<TResult> JoeySelect<TResource, TResult>(this List<TResource> employees, Func<TResource, TResult> selector)
+        {
+            var result = new List<TResult>();
+            foreach (var employee in employees)
+            {
+                result.Add(selector(employee));
             }
 
             return result;
