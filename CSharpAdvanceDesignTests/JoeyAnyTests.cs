115e6748ea55127cdd1330fcc611c19d4ad5d6e8
Ezra <e1z2r3a4@gmail.com>
2019-08-17 09:46:05 +0800

    Add condition in Any, can also use Any(x => x >91) in Linq

--- CSharpAdvanceDesignTests/JoeyAnyTests.cs
+++ CSharpAdvanceDesignTests/JoeyAnyTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using Lab;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -31,6 +32,14 @@ namespace CSharpAdvanceDesignTests
 
             var actual = JoeyAny(emptyEmployees);
             Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void any_number_greater_than_91()
+        {
+            var numbers = new[] { 87, 88, 91, 93, 0 };
+            var actual = numbers.JoeyAnyWithCondition(item => item > 91);
+            Assert.IsTrue(actual);
         }
 
         private bool JoeyAny(IEnumerable<Employee> employees)
