2a21ce168fe3e1bc1a42bd95eb4cf0cdd04390f1
Ezra <e1z2r3a4@gmail.com>
2019-08-18 14:35:50 +0800

    Use IEqualityComparer to solve 2 condition distinct
    Let EmployeeEqualityComparerWithName base on IEqualityComparer and implement the interface to match the condition

--- CSharpAdvanceDesignTests/JoeyDistinctTests.cs
+++ CSharpAdvanceDesignTests/JoeyDistinctTests.cs
@@ -1,8 +1,10 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
 using System.Linq;
+using Lab.Entities;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -33,6 +35,56 @@ namespace CSharpAdvanceDesignTests
             //        yield return current;
             //    }
             //}
+        }
+
+        [Test]
+        public void distinct_employees()
+        {
+            var employees = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+            };
+
+            var actual = JoeyDistinct(employees, new EmployeeEqualityComparerWithName());
+
+            var expected = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<Employee> JoeyDistinct(IEnumerable<Employee> employees, EmployeeEqualityComparerWithName comparar)
+        {
+            var hashSet = new HashSet<Employee>(comparar);
+            var enumerator = employees.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (hashSet.Add(current))
+                {
+                    yield return current;
+                }
+            }
+        }
+    }
+
+    internal class EmployeeEqualityComparerWithName : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee x, Employee y)
+        {
+            return x.FirstName == y.FirstName && x.LastName == y.LastName;
+        }
+
+        public int GetHashCode(Employee obj)
+        {
+            return new { obj.FirstName, obj.LastName }.GetHashCode();
         }
     }
 }
